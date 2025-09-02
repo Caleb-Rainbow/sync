5c0fd7f3865b090a46ce8e753aa561beb0ee5375
杨帅林 <2540040142@qq.com>
2025-09-02 15:48:17 +0800

    ``` feat: Initial commit
    - Add initial Android application structure for 'SyncMoudle'
    - Configure Gradle build scripts, including wrapper, properties, and version catalog
    - Set up basic application manifest, resources, themes, and colors
    - Define ProGuard rules and data extraction/backup configurations
    - Set compile SDK to 36 and min SDK to 28
    - Configure Kotlin and Java compatibility for Java 21
    
    ```

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+
+}
+
+android {
+    namespace = "com.util.sync"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.util.sync"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlin {
+        compilerOptions{
+            jvmTarget = JvmTarget.fromTarget("21")
+        }
+    }
+}
+
+dependencies {
+
+}
